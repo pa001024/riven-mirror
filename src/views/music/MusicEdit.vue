6e3fa7d7163fbc67db628eb34f687da5ca3fd322
pa001024 <pa001024@qq.com>
2019-09-10 01:47:52 +0800

    feat: change tones

--- src/views/music/MusicEdit.vue
+++ src/views/music/MusicEdit.vue
@@ -174,15 +174,15 @@ interface MusicBlock {
 const instrumentResource = {
   piano: {
     path: "/instruments/piano/",
-    keys: ["C4", "D4", "E4", "F4", "G4", "A4", "B4", "C5", "D5", "E5", "F5", "G5", "A5", "B5", "C6", "D6", "E6"],
+    keys: ["C3", "D3", "E3", "F3", "G3", "A3", "B3", "C4", "D4", "E4", "F4", "G4", "A4", "B4", "C5", "D5", "E5"],
   },
   shawzin: {
     path: "/instruments/shawzin/",
-    keys: ["C4", "D4", "E4", "F4", "G4", "A4", "B4", "C5", "D5", "E5", "F5", "G5", "A5", "C6", "D6"],
+    keys: ["C3", "D3", "E3", "F3", "G3", "A3", "B3", "C4", "D4", "E4", "F4", "G4", "A4",  "C5", "D5"],
   },
   lotus: {
     path: "/instruments/lotus/",
-    keys: ["C4", "D4", "E4", "F4", "G4", "A4", "B4", "C5", "D5", "E5", "F5", "G5", "A5", "C6", "D6"],
+    keys: ["C3", "D3", "E3", "F3", "G3", "A3", "B3", "C4", "D4", "E4", "F4", "G4", "A4",  "C5", "D5"],
   },
 };
 
