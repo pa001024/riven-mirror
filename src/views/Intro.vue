75d31895e1c71b9a2fcdbbf4a0f71950573b0b8d
pa001024 <pa001024@qq.com>
2020-10-28 23:24:18 +0800

    fix: download link

--- src/views/Intro.vue
+++ src/views/Intro.vue
@@ -9,7 +9,7 @@
         <div class="action">
           <a class="btn" href="https://warframe.huijiwiki.com/wiki/blog:RivenMirror" target="_blank" v-t="'intro.a1'"></a>
           <router-link class="btn" to="/alerts" v-t="'intro.a2'"></router-link>
-          <a class="btn solid" href="https://pan.baidu.com/s/1GCB7Zh5_VcExijKgixJyCA" target="_blank" v-t="'intro.a3'"></a>(提取码: fja4)
+          <a class="btn solid" href="https://pan.baidu.com/s/1fGX4wJ3SYJKS5xchkMQsZQ" target="_blank" v-t="'intro.a3'"></a>(提取码: 1111)
         </div>
       </div>
     </div>
