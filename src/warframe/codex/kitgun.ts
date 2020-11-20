baef7bad89a815a6960ad755b24e6f97207c232d
pa001024 <pa001024@qq.com>
2020-11-20 16:28:39 +0800

    feat: 1.10.0 U29.5 - NEW KITGUNS

--- src/warframe/codex/kitgun.ts
+++ src/warframe/codex/kitgun.ts
@@ -69,6 +69,7 @@ export interface KitgunLoaderDisplay {
  */
 export interface LoaderChamberData {
   chamberID: string;
+  magazineIndex?: number;
   reload?: number;
   critChance?: number;
   procChance?: number;
@@ -78,49 +79,64 @@ const _kitgunChamber = [
   [0, "Catchmoon", [
     [["Impact", 89], ["Heat", 167]],
     [["Impact", 49], ["Heat", 85]] // Primary
-  ], 0.21, 0.21, [5, 7, 11, 13], 5.9, [0.5, 0.9]],
+  ], 0.21, 0.21, [5, 7, 9, 11, 13, 17, 23, 39], 5.9, [0.5, 0.9]],
   [1, "Gaze", [
     [["Puncture", 6], ["Radiation", 10]],
     [["Radiation", 29]] // Primary
-  ], 0.25, 0.25, [23, 31, 43, 51], 100, [1, 0.85]],
+  ], 0.25, 0.25, [23, 31, 37, 43, 51, 49, 85, 113], 100, [1, 0.85]],
   [2, "Rattleguts", [
     [["Impact", -1], ["Puncture", 7], ["Slash", 5], ["Radiation", 10]],
     [["Impact", 2], ["Puncture", 5], ["Slash", 3], ["Radiation", 9]] // Primary
-  ], 0.19, 0.19, [29, 45, 67, 83], 26, [0.75, 0.8]],
+  ], 0.19, 0.19, [29, 45, 57, 67, 83, 75, 111, 153], 26, [0.75, 0.8]],
   [3, "Tombfinger", [
     [["Impact", 12], ["Puncture", 5], ["Radiation", 43]],
     [["Puncture", 40], ["Radiation", 18]] // Primary
-  ], 0.24, 0.24, [9, 15, 23, 29], 50, [0.65, 0.75]]
+  ], 0.24, 0.24, [9, 15, 19, 23, 29, 31, 35, 49], 50, [0.65, 0.75]],
+  [4, "Sporelacer", [
+    [["Impact", -39], ["Toxin", 103]],
+    [["Impact", 21], ["Toxin", 69]] // Primary
+  ], 0.21, 0.21, [5, 7, 9, 11, 13, 17, 23, 39], 5.9, [0.5, 0.5]],
+  [5, "Vermisplicer", [
+    [["Impact", 1.5], ["Puncture", 3.5], ["Slash", 4.5], ["Toxin", 4.5]],
+    [["Impact", 5], ["Puncture", 6], ["Slash", 10], ["Toxin", 12]] // Primary
+  ], 0.25, 0.25, [23, 31, 37, 43, 51, 49, 85, 113], 5.4, [0.5, 0.5]],
 ] as [number, string, [string, number][][], number, number, number[], number, number[]][];
 
 const _kitgunGrip = [
-  [0, "Gibber", [[-48, 190], [0, 720], [0, 660], [-4, 270]], "Secondary"],
-  [1, "Ramble", [[-24, 150], [0.5, 720], [1.5, 530], [0, 220]], "Secondary"],
-  [2, "Lovetap", [[51, 90], [1.5, 720], [7.8, 310], [12, 150]], "Secondary"],
-  [3, "Haymaker", [[102, 70], [2, 720], [12.5, 220], [20, 130]], "Secondary"],
-  [4, "Brash", [[-12, 340], [0, 480], [-0.5, 1020], [-10, 226]], "Primary"],
-  [5, "Shrewd", [[-6, 280], [2, 480], [-0.2, 840], [-5, 188]], "Primary"],
-  [6, "Steadyslam", [[20.5, 200], [6, 480], [2.3, 600], [14.5, 140]], "Primary"],
-  [7, "Tremor", [[41, 180], [8, 480], [4.5, 540], [29, 128]], "Primary"]
+  [0, "Gibber", [[-48, 190], [0, 720], [0, 660], [-4, 270], [0, 190], [0, 720]], "Secondary"],
+  [1, "Ramble", [[-24, 150], [0.5, 720], [1.5, 530], [0, 220], [48, 150], [0.3, 720]], "Secondary"],
+  [2, "Ulnaris", [[17, 110], [1, 720], [3.5, 400], [6.7, 170], [130, 110], [0.5, 720]], "Secondary"],
+  [3, "Lovetap", [[51, 90], [1.5, 720], [7.8, 310], [12, 150], [198, 90], [0.8, 720]], "Secondary"],
+  [4, "Haymaker", [[102, 70], [2, 720], [12.5, 220], [20, 130], [300, 70], [1, 720]], "Secondary"],
+
+  [5, "Brash", [[-12, 340], [0, 480], [-0.5, 1020], [-10, 226], [0, 340], [0, 480]], "Primary"],
+  [6, "Palmaris", [[-12, 340], [1, 480], [-0.3, 930], [-7.5, 207], [6, 310], [0.3, 480]], "Primary"],
+  [7, "Shrewd", [[-6, 280], [2, 480], [-0.2, 840], [-5, 188], [12, 280], [0.5, 480]], "Primary"],
+  [8, "Steadyslam", [[20.5, 200], [6, 480], [2.3, 600], [14.5, 140], [65, 200], [1.5, 480]], "Primary"],
+  [9, "Tremor", [[41, 180], [8, 480], [4.5, 540], [29, 128], [106, 180], [2, 480]], "Primary"]
 ] as [number, string, [number, number][], string][];
 
 const _kitgunLoader = [
   [0, "Flutterfire", -0.3, -0.08, 0.14, 0, 1, []],
-  [1, "Ramflare", -0.3, -0.08, 0.14, 0.4, 2, []],
+  [1, "Ramflare", -0.3, -0.08, 0.14, 0.4, 3, []],
   [2, "Zipfire", -0.1, -0.04, 0.07, -0.4, 0, []],
   [3, "Swiftfire", -0.1, -0.04, 0.07, 0, 1, []],
-  [4, "Sparkfire", -0.1, -0.04, 0.07, 0.4, 2, []],
-  [5, "Thunderdrum", -0.1, -0.04, 0.07, 0.8, 3, [{ chamberID: "Catchmoon", critChance: -0.08, procChance: 0.14 }]],
+  [4, "Sparkfire", -0.1, -0.04, 0.07, 0.4, 3, []],
+  [5, "Thunderdrum", -0.1, -0.04, 0.07, 0.8, 4, [{ chamberID: "Catchmoon", critChance: -0.08, procChance: 0.14 }]],
   [6, "Zip", 0, 0, 0, -0.4, 0, []],
   [7, "Slap", 0, 0, 0, 0, 1, []],
-  [8, "Deepbreath", 0, 0, 0, 0.4, 2, []],
-  [9, "Bellows", 0, 0, 0, 0.8, 3, []],
+  [8, "Deepbreath", 0, 0, 0, 0.4, 3, []],
+  [9, "Bellows", 0, 0, 0, 0.8, 4, []],
   [10, "Zipneedle", 0.1, 0.07, -0.04, -0.4, 0, []],
   [11, "Slapneedle", 0.1, 0.07, -0.04, 0, 1, []],
-  [12, "Bashrack", 0.1, 0.07, -0.04, 0.4, 2, []],
-  [13, "Stitch", 0.1, 0.07, -0.04, 0.8, 3, []],
+  [12, "Bashrack", 0.1, 0.07, -0.04, 0.4, 3, []],
+  [13, "Stitch", 0.1, 0.07, -0.04, 0.8, 4, []],
   [14, "Killstream", 0.3, 0.14, -0.08, 0, 1, []],
-  [15, "Splat", 0.3, 0.14, -0.08, 0.4, 2, []]
+  [15, "Splat", 0.3, 0.14, -0.08, 0.4, 3, []],
+  [16, "Arcroid", 0, 0.03, 0.03, 0.2, 2, []],
+  [17, "Thymoid", 0, 0, 0, 1, 5, []],
+  [18, "Marco Arcroid", 0, 0.03, 0.03, 1.6, 6, []],
+  [19, "Marco Thymoid", 0, 0, 0, 1.8, 7, []],
 ] as [number, string, number, number, number, number, number, LoaderChamberData[]][];
 
 export const KitgunChamberData: KitgunChamber[] = _kitgunChamber.map(v => ({
@@ -159,7 +175,7 @@ export const KitgunLoaderData: KitgunLoader[] = _kitgunLoader.map(v => ({
   chambersData: v[7]
 }));
 
-export const NoneKitgunGripData: number[] = [3, 0, 2, 3];
+export const NoneKitgunGripData: number[] = [4, 0, 3, 4, 4, 4];
 
 export const NoneKitgunLoaderData: KitgunLoader = KitgunLoaderData[14];
 
@@ -224,11 +240,14 @@ export class Kitgun extends Weapon {
     mode.damage = dmgs
       .map(([n, v]) => [n, hAccSum(v, (this.name === "Tombfinger" && this.grip.type === "Secondary" && n === "Radiation" ? 4 : 1) * grip.dmgAdd)] as [string, number])
       .filter(v => v[1] > 0);
-    mode.critMul = hAccSum(loader.critDamage, 2);
+    mode.critMul = hAccSum(loader.critDamage, this.chamber.name === "Sporelacer" && this.grip.type === "Primary" ? 3 : 2);
     mode.critChance = hAccSum(this.chamber.critChance, loader.critChance);
     mode.procChance = hAccSum(this.chamber.procChance, loader.procChance);
     mode.fireRate = grip.fireRate;
     mode.accuracy = this.chamber.accuracy;
+    if (this.chamber.name === "Vermisplicer" && this.grip.type === "Secondary") {
+      mode.pellets = 5;
+    }
     this.reload = hAccSum(1.3, loader.reload);
     this.magazine = loader.magazine;
     switch (this.name) {
